926f2687eeac02ab7655c58ede2fd5fa2269f922
sebacoma <sebacomaia@gmail.com>
2025-06-21 19:10:53 -0400

    feat: enable CORS for development in main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    // Just for Development, adjust as needed
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
