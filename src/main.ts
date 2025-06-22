b4e843fbcc81751e540a5a3ebdd4327787b080b7
PedroSebastian <pizarropedrov@gmail.com>
2025-06-21 22:23:20 -0400

    fix: add Authorization header to CORS allowedHeaders in main application

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
